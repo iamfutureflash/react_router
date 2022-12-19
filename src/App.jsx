9e270c1cbda2b730f731d33be848c9e4ce9aabf1
iamfutureflash <xenode2000@gmail.com>
2022-12-19 10:48:25 +0530

    error page not found added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Home'
 import Contact from './Contact'
 import About from './About'
 import Navbar from './Navbar'
+import Error from './Error'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="contact" element={<Contact/>} />
           <Route path="about" element={<About/>} />
+          <Route path="*" element={<Error/>} />
         </Routes>
       </BrowserRouter>
     </>
